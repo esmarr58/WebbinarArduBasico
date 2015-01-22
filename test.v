49ed426259bd1997580f86a7dfc8f0d4ec008074
Hector Torres <hector.torres@hetpro.com.mx>
2015-01-21 22:25:54 -0600

    Create test.v

---
+++ test.v
@@ -0,0 +1,18 @@
+module test(cristal,clk_1s);
+	input cristal;
+	output reg clk_1s = 1;
+	
+	reg[24:0] contador = 0;
+	
+	always @(posedge cristal)
+	begin
+		contador = contador + 1;
+		if(contador == 25_000_000)
+			begin
+				contador = 0;
+				clk_1s = ~clk_1s;
+			end
+	end
+	
+	
+endmodule
